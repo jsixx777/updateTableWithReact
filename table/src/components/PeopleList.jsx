c7d1c3c7c72c29e972e4b46618b0487c6d38436e
JPSiler <jsixx77@hotmail.com>
2021-11-17 19:13:02 -0600

    make successful api call and log response

--- table/src/components/PeopleList.jsx
+++ table/src/components/PeopleList.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 
-export default function PeopleList () {
+export default function PeopleList() {
+
+    useEffect(() => {
+        fetch("https://ghibliapi.herokuapp.com/people")
+            .then(res => res.json())
+            .then(json => console.log(json))
+    }, []);
+
+
     return (
         <div className="main">
             <h3 className="title">My List</h3>
