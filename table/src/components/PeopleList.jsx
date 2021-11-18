3a77b1748211d1e6ae8f4cf8c6575ba3669a5c49
JPSiler <jsixx77@hotmail.com>
2021-11-17 19:22:37 -0600

    declare first state and set up table with basic styling

--- table/src/components/PeopleList.jsx
+++ table/src/components/PeopleList.jsx
@@ -3,16 +3,37 @@ import '../App.css';
 
 export default function PeopleList() {
 
+    const [people, setPeople] = useState([]);
+
     useEffect(() => {
         fetch("https://ghibliapi.herokuapp.com/people")
             .then(res => res.json())
-            .then(json => console.log(json))
+            .then(json => setPeople(json))
     }, []);
 
 
     return (
         <div className="main">
-            <h3 className="title">My List</h3>
+            <table className="table-table">
+                <thead>
+                    <tr className="even-row">
+                        <th>Name</th>
+                        <th>Gender</th>
+                        <th>Age</th>
+                        <th>Eye Color</th>
+                        <th>Hair Color</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="even-row">
+                        <td>Bob</td>
+                        <td>Male</td>
+                        <td>35</td>
+                        <td>Blue</td>
+                        <td>Purple</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
