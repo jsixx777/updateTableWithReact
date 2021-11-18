09b0baffea1540312e26d96365dde9e930e219b0
JPSiler <jsixx77@hotmail.com>
2021-11-17 19:48:39 -0600

    set up state for adding data and clear inputs when each name added

--- table/src/components/PeopleList.jsx
+++ table/src/components/PeopleList.jsx
@@ -4,6 +4,13 @@ import '../App.css';
 export default function PeopleList() {
 
     const [people, setPeople] = useState([]);
+    const [addNewPeople, setAddNewPeople] = useState({
+        name: "",
+        gender: "",
+        age: "",
+        eye_color: "",
+        hair_color: ""
+    })
 
     useEffect(() => {
         fetch("https://ghibliapi.herokuapp.com/people")
@@ -11,9 +18,91 @@ export default function PeopleList() {
             .then(json => setPeople(json))
     }, []);
 
+    const handleAddNewPeopleChange = (e) => {
+        e.preventDefault();
+
+        const fieldName = e.target.getAttribute('name');
+        const fieldValue = e.target.value;
+
+        const newUpdatedFormData = { ...addNewPeople }
+        newUpdatedFormData[fieldName] = fieldValue;
+
+        setAddNewPeople(newUpdatedFormData);
+    }
+
+    const handleAddNewPeopleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const newPerson = {
+            name: addNewPeople.name,
+            gender: addNewPeople.gender,
+            age: addNewPeople.age,
+            eye_color: addNewPeople.eye_color,
+            hair_color: addNewPeople.hair_color
+        }
+
+        const newPeople = [...people, newPerson];
+        setPeople(newPeople);
+        setAddNewPeople({
+            name: "",
+            gender: "",
+            age: "",
+            eye_color: "",
+            hair_color: ""
+        })
+    }
 
     return (
         <div className="main">
+            <form onSubmit={handleAddNewPeopleFormSubmit} className="form-form">
+                <input
+                    type="text"
+                    value={addNewPeople.name}
+                    onChange={handleAddNewPeopleChange}
+                    className="input-field"
+                    name="name"
+                    required="required"
+                    placeholder="Enter name..."
+                ></input>
+                <input
+                    type="text"
+                    value={addNewPeople.gender}
+                    className="input-field"
+                    onChange={handleAddNewPeopleChange}
+                    name="gender"
+                    required="required"
+                    placeholder="Enter gender..."
+                ></input>
+                <input
+                    type="text"
+                    value={addNewPeople.age}
+                    className="input-field"
+                    onChange={handleAddNewPeopleChange}
+                    name="age"
+                    required="required"
+                    placeholder="Enter age..."
+                ></input>
+                <input
+                    type="text"
+                    className="input-field"
+                    value={addNewPeople.eye_color}
+                    onChange={handleAddNewPeopleChange}
+                    name="eye_color"
+                    required="required"
+                    placeholder="Enter Eye Color..."
+                ></input>
+                <input
+                    type="text"
+                    value={addNewPeople.hair_color}
+                    className="input-field"
+                    onChange={handleAddNewPeopleChange}
+                    name="hair_color"
+                    required="required"
+                    placeholder="Enter Hair Color..."
+                ></input>
+                <button type="submit" className="add-name-button">Submit</button>
+            </form>
+
             <table className="table-table">
                 <thead>
                     <tr className="even-row">
